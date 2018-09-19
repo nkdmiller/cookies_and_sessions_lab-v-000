907d33d1c7249769c2e9336da35caf9a8a83aa26
Nate Miller <natekmiller96@gmail.com>
2018-09-19 03:13:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   def cart
+<<<<<<< HEAD
     session[:cart] ||= []
+=======
+    session[:cart]
+>>>>>>> 9164cc4b5c4793312b91f48440833c12e111aa13
   end
   protect_from_forgery with: :exception
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,20 @@
 class ProductsController < ApplicationController
   def index
+<<<<<<< HEAD
     @products = cart
   end
 
   def add
     cart << params[:product]
     @products = cart
+=======
+    @products = session[:cart] || []
+  end
+
+  def add
+    session[:cart] = session[:cart] || []
+    session[:cart] << params[:item]
+>>>>>>> 9164cc4b5c4793312b91f48440833c12e111aa13
     render :index
   end
 end
